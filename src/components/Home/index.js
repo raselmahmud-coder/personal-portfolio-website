790c1ca007562177fc56d08a77912b34b731220a
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-06-09 23:30:04 +0800

    logo home page add

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,17 +2,16 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
 import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = 'udip'.split('')
-  const jobArray = 'Web Developer'.split('')
-  const interestArray = 'AI & Blockchain'.split('')
-  const enthusiastArray = 'Enthusiast'.split('')
+  const nameArray = 'Rasel'.split('')
+  const jobArray = 'Full Stack'.split('')
+  const interestArray = 'JavaScript'.split('')
+  const enthusiastArray = 'Engineer'.split('')
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -31,10 +30,7 @@ const Home = () => {
             <br />
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
+            <br />
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -61,10 +57,13 @@ const Home = () => {
             />
           </h1>
           <h2>
-            Full Stack Developer / AI and Blockchain Enthusiast / Avid Learner
+            Full Stack Developer / JavaScript Engineer / Quick Learner
           </h2>
-          <Link to="/contact" className="flat-button">
+          <Link to="/contact" className="flat-button" style={{marginRight:"5px"}}>
             CONTACT ME
+          </Link>
+          <Link to="/" className="flat-button">
+            RESUME
           </Link>
         </div>
         <Logo />
